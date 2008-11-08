5d78631c41de39b8635243fe902b3a5b1897dc41
Gnarfoz <gnarfoz@gmail.com>
2008-11-08 18:38:19 +0000

    - basically rewrite the 'entire thing', it's only a couple of lines anyway - removed Ace2 - used ChatFrame_AddMessageEventFilter from patch 2.4 instead

--- DontBugMe.lua
+++ DontBugMe.lua
@@ -1,25 +1,13 @@
-----------------------------
---      Declaration       --
-----------------------------
+local data = {}
 
-DontBugMe = AceLibrary("AceAddon-2.0"):new("AceHook-2.1")
-
-----------------------------
---      Main Functions    --
-----------------------------
-
-function DontBugMe:OnEnable()
-	self.data = {}
-	self:Hook("ChatFrame_OnEvent", "FilterMessage", true)
+local function filter(msg, a2)
+    a2 = a2 or arg2     -- be future proof for when Judgement Day comes for argN globals
+    if data[a2] and data[a2] == msg then
+        return true
+    else
+        data[a2] = msg
+    end
 end
 
-function DontBugMe:FilterMessage(event)
-	if ( event == "CHAT_MSG_AFK" or event == "CHAT_MSG_DND") then
-		if self.data[arg2] and self.data[arg2] ~= arg1 or not self.data[arg2] then
-			self.data[arg2] = arg1
-			self.hooks.ChatFrame_OnEvent(event)
-		end
-	else
-		self.hooks.ChatFrame_OnEvent(event)
-	end
-end
+ChatFrame_AddMessageEventFilter("CHAT_MSG_AFK", filter)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_DND", filter)
